f735dde76788b951cd41504cd916a00ede78efc8
Soulo <verma.yashaswi@gmail.com>
2025-07-12 18:09:29 +0530

    post local changes on studio

--- android/app/src/main/java/app/soulo/online/MainActivity.java
+++ android/app/src/main/java/app/soulo/online/MainActivity.java
@@ -1,21 +1,12 @@
-
 package app.soulo.online;
 
-import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import com.getcapacitor.Plugin;
 import com.codetrix.studio.CapacitorGoogleAuth.GoogleAuth;
-
-import java.util.ArrayList;
 
 public class MainActivity extends BridgeActivity {
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(android.os.Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
-        // Initialize plugins
-        this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
-            add(GoogleAuth.class);
-        }});
+        registerPlugin(GoogleAuth.class);
     }
 }
